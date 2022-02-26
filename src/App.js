f9608992e1fa761c116e5278b90eca6f0e1205f9
PufflyMan <ahmedovyusif8785@gmail.com>
2022-02-26 23:56:55 +0400

    props update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Navbar from './Navbar.js';
 import Hero from './Hero';
 import Card from './Card';
+import img from './images/image12.png';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={img}
+        rating={"5.0"}
+        title={"Life Lessons with Katie Zaferes"}
+        price={"136"}
+        country={"USA"}
+        review={"6"}
+      />
     </div>
   )
 }
